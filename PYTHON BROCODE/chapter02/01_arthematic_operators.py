ae7e296e0958210ceb58bcfe8f881aeeba7bb8f2
shahrukh051 <shahrukhyt01@gmail.com>
2025-11-11 15:01:30 +0530

    Add files via upload

---
+++ PYTHON BROCODE/chapter02/01_arthematic_operators.py
@@ -0,0 +1,27 @@
+#arthmatic operators
+
+### addition
+
+friends = 5
+#friends = friends +1
+friends += 1
+
+###subpraction
+#friends = friends - 2
+friends -= 2
+
+###multipication
+#friends = friends * 3
+friends *= 3
+
+###division
+# friends = friends / 2
+friends /= 2
+
+#friends = friends **2
+friends **= 2
+
+###modulas 
+remainder = friends % 2
+
+print(remainder)
