10929c46a069aa6356b5e2f3459202dc9d619fc0
shahrukh051 <shahrukhyt01@gmail.com>
2025-11-11 15:15:12 +0530

    Delete PYTHON BROCODE directory

--- PYTHON BROCODE/chapter01/exercise01.py
+++
@@ -1,9 +0,0 @@
-# Exercise 1 rectangular area calc 
-
-length = float(input("Enter the length"))
-width = float(input("Enter the width"))
-area = length * width 
-
-print(F"the area is: {area}cm²")
-
-
--- PYTHON BROCODE/chapter02/exercise_01_radius_circle.py
+++
@@ -1,6 +0,0 @@
-import math
-
-radius = float(input('rnter the radious of a circle:'))
-circumference = 2 * math.pi * radius
-
-print(f"The circumference is : {round(circumference,2)}")
